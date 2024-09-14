2ee45b54d81721b6eaea709390d94236b1c02374
okthefolder <ohto.katila@gmail.com>
2024-09-14 16:47:00 +0300

    merged

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <ncurses.h>
 #include <vector>
 #include <iostream>
+#include <unistd.h>
+#include <map>
 
 class Player {
     public: 
@@ -20,6 +22,15 @@ int tileHeight = 8;
 int tileWidth = 16;
 int height = 9 * tileHeight;
 int width = 18 * tileWidth;
+
+
+std::vector<std::vector<int>> spriteSizes = {
+    {16,8},
+    {16,8},
+    {16,8},
+    {16,8},
+    {8,4},
+};
 
 std::vector<std::vector<short>> spriteColorFront = {
     {
@@ -152,8 +163,66 @@ std::vector<std::vector<int>> sprites= {
         1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1,
         1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1,
         1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-    }
+
+    },
+    {
+        1,1,1,1,1,1,1,1,
+        1,1,1,1,1,1,1,1,
+        1,1,1,1,1,1,1,1,
+        1,1,1,1,1,1,1,1,
+
+    },
 };
+
+std::vector<std::vector<short>> spriteColorFront = {
+    {
+        196,196,195,195,195,195,195,195,195,195,195,195,195,195,195,195,
+        195,196,196,195,195,195,195,195,195,195,195,195,195,195,195,195,
+        195,195,196,196,195,195,195,195,195,195,195,195,195,195,195,195,
+        195,195,195,195,195,195,195,195,195,195,195,195,195,195,195,195,
+        195,195,195,195,195,195,195,195,195,195,195,195,195,195,195,195,
+        195,195,195,195,195,195,195,195,195,195,195,195,195,195,195,195,
+        195,195,195,195,195,195,195,195,195,195,195,195,195,195,195,195,
+        195,195,195,195,195,195,195,195,195,195,195,195,195,195,195,195,
+    },
+        {
+        196,196,195,195,195,195,195,195,195,195,195,195,195,195,195,195,
+        195,196,196,195,195,195,195,195,195,195,195,195,195,195,195,195,
+        195,195,196,196,195,195,195,195,195,195,195,195,195,195,195,195,
+        195,195,195,195,195,195,195,195,195,195,195,195,195,195,195,195,
+        195,195,195,195,195,195,195,195,195,195,195,195,195,195,195,195,
+        195,195,195,195,195,195,195,195,195,195,195,195,195,195,195,195,
+        195,195,195,195,195,195,195,195,195,195,195,195,195,195,195,195,
+        195,195,195,195,195,195,195,195,195,195,195,195,195,195,195,195,
+    },
+            {
+        196,196,195,195,195,195,195,195,195,195,195,195,195,195,195,195,
+        195,196,196,195,195,195,195,195,195,195,195,195,195,195,195,195,
+        195,195,196,196,195,195,195,195,195,195,195,195,195,195,195,195,
+        195,195,195,195,195,195,195,195,195,195,195,195,195,195,195,195,
+        195,195,195,195,195,195,195,195,195,195,195,195,195,195,195,195,
+        195,195,195,195,195,195,195,195,195,195,195,195,195,195,195,195,
+        195,195,195,195,195,195,195,195,195,195,195,195,195,195,195,195,
+        195,195,195,195,195,195,195,195,195,195,195,195,195,195,195,195,
+    },
+            {
+        196,196,195,195,195,195,195,195,195,195,195,195,195,195,195,195,
+        195,196,196,195,195,195,195,195,195,195,195,195,195,195,195,195,
+        195,195,196,196,195,195,195,195,195,195,195,195,195,195,195,195,
+        195,195,195,195,195,195,195,195,195,195,195,195,195,195,195,195,
+        195,195,195,195,195,195,195,195,195,195,195,195,195,195,195,195,
+        195,195,195,195,195,195,195,195,195,195,195,195,195,195,195,195,
+        195,195,195,195,195,195,195,195,195,195,195,195,195,195,195,195,
+        195,195,195,195,195,195,195,195,195,195,195,195,195,195,195,195,
+    },
+    {
+        196,196,196,196,196,196,196,196,
+        196,196,196,196,196,196,196,196,
+        196,196,196,196,196,196,196,196,
+        196,196,196,196,196,196,196,196,
+    },
+};
+
 std::vector<std::vector<short>> spriteColorBack = {
     {
         0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
@@ -196,25 +265,11 @@ std::vector<std::vector<short>> spriteColorBack = {
         0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
     },
     {
-        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        196,196,196,196,196,196,196,196,
+        196,196,196,196,196,196,196,196,
+        196,196,196,196,196,196,196,196,
+        196,196,196,196,196,196,196,196,
     },
-    {
-        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    }
 };
 
 /* 0: ilma
@@ -239,14 +294,18 @@ std::vector<int> tileMap = {
 
 std::vector<char> characters = {' ', '#', 'b', '/', '|', '\\', '-', '_', '(', ')', 'o', '<', '>'};
 
+std::map<int,int> colorIndexes;
+
 void draw_sprite(WINDOW *win, int screenInfo[], int spriteIndex, int x, int y){
-    for (int y0=0; y0<screenInfo[2];y0++){
-        for (int x0=0; x0<screenInfo[3];x0++){
-            int characterIndex = sprites[spriteIndex][y0*screenInfo[3]+x0]; 
-            int pairIndex=spriteIndex*screenInfo[3]*screenInfo[2]+y0*screenInfo[3]+x0;
+    for (int y0=0; y0<spriteSizes[spriteIndex][1];y0++){
+        for (int x0=0; x0<spriteSizes[spriteIndex][0];x0++){
+            int characterIndex = sprites[spriteIndex][y0*spriteSizes[spriteIndex][0]+x0]; 
+            int fc = spriteColorFront[spriteIndex][y0*spriteSizes[spriteIndex][0]+x0];
+            int bc = spriteColorBack[spriteIndex][y0*spriteSizes[spriteIndex][0]+x0];
+            int pairIndex = colorIndexes[(bc<<16)|fc];
             wattron(win, COLOR_PAIR(pairIndex));
             mvwaddch(win, y0+y, x0+x,characters[characterIndex]);
-            wattroff(win, COLOR_PAIR(1));
+            wattroff(win, COLOR_PAIR(pairIndex));
         }
     }
 }
@@ -255,10 +314,12 @@ void draw_tile(WINDOW *win, int screenInfo[],int tileIndex, int x, int y){
     for (int y0=0; y0<screenInfo[2];y0++){
         for (int x0=0; x0<screenInfo[3];x0++){
             int characterIndex = sprites[tileIndex][y0*screenInfo[3]+x0]; 
-            int pairIndex=tileIndex*screenInfo[3]*screenInfo[2]+y0*screenInfo[3]+x0;
+            int fc = spriteColorFront[tileIndex][y0*spriteSizes[tileIndex][0]+x0];
+            int bc = spriteColorBack[tileIndex][y0*spriteSizes[tileIndex][0]+x0];
+            int pairIndex = colorIndexes[(bc<<16)|fc];
             wattron(win, COLOR_PAIR(pairIndex));
             mvwaddch(win, y0+y*screenInfo[2], x0+x*screenInfo[3], characters[characterIndex]);
-            wattroff(win, COLOR_PAIR(1));
+            wattroff(win, COLOR_PAIR(pairIndex));
         }
     }
 }
@@ -293,10 +354,16 @@ void handleInput() {
     }
 }
 
-void utilize_colors(){
+void utilize_colors(WINDOW *win){
+    int n=0;
     for (int i=0;i<sprites.size(); i++){
+        //spriteColorIndex.push_back({});
         for (int j=0;j<sprites[i].size();j++){
-            init_pair(i*sprites[i].size()+j, spriteColorFront[i][j], spriteColorBack[i][j]);
+            if (colorIndexes.find((spriteColorBack[i][j]<<16) | (spriteColorFront[i][j]))==colorIndexes.end()){
+                colorIndexes[(spriteColorBack[i][j]<<16)|(spriteColorFront[i][j])]=n;
+                init_pair(n, spriteColorFront[i][j], spriteColorBack[i][j]);
+                n++;
+            }
         }
     }
 }
@@ -308,12 +375,13 @@ int main(){
     curs_set(0);
     start_color();
     nodelay(stdscr, TRUE);
-    utilize_colors();
     player = new Player(tileHeight * 2, tileWidth * 2, 0);
     int heightTiles = height/tileHeight;
     int widthTiles = width/tileWidth;
     int screenInfo[6]={height, width, tileHeight, tileWidth, heightTiles, widthTiles};
     WINDOW* win = newwin(height, width, 0, 0);
+    utilize_colors(win);
+
     int ch;
     while(true){
         update_screen(win, screenInfo);
