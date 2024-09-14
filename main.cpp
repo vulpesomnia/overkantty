6c012e59ec58616f4ddcef940ed0bdb554b8f922
okthefolder <96258740+okthefolder@users.noreply.github.com>
2024-09-14 10:13:54 +0300

    rendering fix

--- main.cpp
+++ main.cpp
@@ -4,18 +4,41 @@
 
 std::vector<std::vector<int>> tiles= {
     {
-        0,0,0,0,0,
-        0,0,0,0,0,
-        0,0,0,0,0,
-        0,0,0,0,0,
-        0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
     },
     {
-        1,1,1,1,1,
-        1,1,1,1,1,
-        1,1,1,1,1,
-        1,1,1,1,1,
-        1,1,1,1,1,
+        1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+        1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+        1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+        1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+        1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+        1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+        1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+        1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+        1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+        1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+        1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+        1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+        1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+        1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+        1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+        1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+
     },
 };
 
@@ -42,7 +65,7 @@ void draw_sprite(WINDOW *win, int screenInfo[], int spriteIndex, int x, int y){
     for (int y0=0; y0<screenInfo[2];y0++){
         for (int x0=0; x0<screenInfo[3];x0++){
             int characterIndex = sprites[spriteIndex][y0*screenInfo[2]+x0]; 
-            mvwaddch(win, x0+x, y0+y,characters[characterIndex]);
+            //mvwaddch(win, x0+x, y0+y,characters[characterIndex]);
             //mvwaddch(win, 1, 1,'a');
 
         }
@@ -54,14 +77,16 @@ void draw_tile(WINDOW *win, int screenInfo[],int tileIndex, int x, int y){
     for (int y0=0; y0<screenInfo[2];y0++){
         for (int x0=0; x0<screenInfo[3];x0++){
             int characterIndex = tiles[tileIndex][y0*screenInfo[2]+x0]; 
-            mvwaddch(win, x0+x*screenInfo[3], y0+y*screenInfo[2],characters[characterIndex]);
+            mvwaddch(win, y0+y*screenInfo[2], x0+x*screenInfo[3], characters[characterIndex]);
         }
     }
 }
 
 void update_screen(WINDOW *win, int screenInfo[], int tileMap[]){
+    //std::cout<<"hi"<<std::endl;
     for (int y=0;y<screenInfo[4];y++){
         for (int x=0; x<screenInfo[5];x++){
+            //std::cout<<"hi"<<std::endl;
             draw_tile(win, screenInfo, tileMap[y*screenInfo[4] + x], x, y);
         }
     }
@@ -74,12 +99,15 @@ int main(){
     curs_set(0);
     nodelay(stdscr, TRUE);
 
-    int height = 20;
-    int width = 20;
-    int tileHeight = 4;
-    int tileWidth = 4;
+    int height=160;
+    int width=40;
+    getmaxyx(stdscr, height, width);
+    int tileHeight = 8;
+    int tileWidth = 16;
     int heightTiles = height/tileHeight;
-    int widthTiles = height/tileWidth;
+    int widthTiles = width/tileWidth;
+    //heightTiles-=5;
+    //widthTiles-=5;
     int screenInfo[6]={height, width, tileHeight, tileWidth, heightTiles, widthTiles};
     int tileMap[heightTiles*widthTiles];
     for (int i=0; i<heightTiles*widthTiles;i++){
